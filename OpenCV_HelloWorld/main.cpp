4b86e355d6fa2bdb087ed86e5d626bb1ec9919b4
Isaac Wiebe <Isaac.wiebe@me.com>
2021-05-01 19:15:46 -0400

    Initial Commit

---
+++ OpenCV_HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenCV_HelloWorld
+//
+//  Created by Isaac Wiebe on 2021-05-01.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
