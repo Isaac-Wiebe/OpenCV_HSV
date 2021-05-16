91e43e416a40441fa0d97b38fd5adede6cf0cbd3
Isaac Wiebe <Isaac.wiebe@me.com>
2021-05-16 19:47:35 -0400

    Changes file names

--- OpenCV_Matrices/main.cpp
+++ OpenCV_Matrices/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  OpenCV_AffineTransforms
+//  OpenCV_Blending
 //
 //  Created by Isaac Wiebe on 2021-05-01.
 //
@@ -10,7 +10,7 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
-// docs.opencv.org/4.5.2/d7/d37/tutorial_mat_mask_operations.html tutorial
+// docs.opencv.org/4.5.2/d5/dc4/tutorial_adding_images.html tutorial
 
 
 using namespace cv;
@@ -29,56 +29,14 @@ int main(int argc, const char * argv[])
         
         // Image setup
        
-        String path = getTruePath("licenseplate_motion.jpg");
-        Mat img = imread(path);
-        Mat dest, dest_dirFilt, dest_gaussFilt;
-       
+        String path = getTruePath("lena.jpg");
+        Mat img1 = imread(path);
         
-        imshow("motion.jpg", img);
-        
+        path = getTruePath("opencv-logo-white.png");
+        Mat img2 = imread(path);
         
         
-        int k = waitKey(0);
-        
-        
-        Mat kernel = (Mat_<double>(3, 3) << 0.111, 0.111,  0.111,
-                      0.111,  0.111, 0.111,
-                     0.111, 0.111,  0.111);
-        
-        filter2D(img, dest, img.depth(), kernel);
-        
-        Mat kernel_dir = (Mat_<double>(3, 3) << 1./16, 1./8, 1./16,
-                      1./8, 1./4, 1./8,
-                      1./16, 1./8, 1./16);
-        
-        filter2D(img, dest_dirFilt, img.depth(), kernel_dir);
-        
-        Mat gauss_kernel = (Mat_<double>(7, 7) << 1., 4., 7., 10., 7., 4., 1.,
-                            4., 12., 26., 33., 26., 12., 4.,
-                            7., 26., 55., 71., 55., 26., 7.,
-                            10., 33., 71., 91., 71., 33., 10.,
-                            7., 26., 55., 71., 55., 26., 7.,
-                            4., 12., 26., 33., 26., 12., 4.,
-                            1., 4., 7., 10., 7., 4., 1.
-                            );
-        
-        gauss_kernel = gauss_kernel/1115;
-        
-        
-        filter2D(img, dest_gaussFilt, img.depth(), gauss_kernel );
-        
-        
-        
-        
-        imshow("Averaging filter", dest); // Averaging filter
-        waitKey();
-        
-        imshow("Directional Averaging filter", dest_dirFilt);
-        waitKey();
-        
-        imshow("Gaussian filter", dest_gaussFilt);
-        waitKey();
-        
+
        
         return 0;
     }
